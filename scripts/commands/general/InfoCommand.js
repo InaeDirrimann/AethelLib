2cdc971e564d078ad8dac5f2bee4d2bfadcd806d
root <root@localhost.localdomain>
2026-09-24 14:18:00 +0000

    chore(release): set version to 1.1.5 and target game version 1.26.51

--- scripts/commands/general/InfoCommand.js
+++ scripts/commands/general/InfoCommand.js
@@ -76,5 +76,5 @@ function getUptime() {
 // | returns the hardcoded release string for the current deployment.          |
 // ----------------------------------------------------------------------------
 function getServerVersion() {
-    return "1.5.0 (Bedrock 1.26.51)"
+    return "1.1.5 (Bedrock 1.26.51)"
 }
